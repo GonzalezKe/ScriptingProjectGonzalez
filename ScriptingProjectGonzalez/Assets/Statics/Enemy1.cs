9c4d2a2d898ca275787338852fc2d2f62606bcf6
GonzalezKe <710293@student.dusd.net>
2024-01-30 13:20:20 -0800

    statics

---
+++ ScriptingProjectGonzalez/Assets/Statics/Enemy1.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour
+{
+
+    public static int enemyCount = 0;
+
+    public Enemy1()
+    {
+        enemyCount++;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ScriptingProjectGonzalez/Assets/Statics/Game1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemy1 enemy1 = new Enemy1();
+        Enemy1 enemy2 = new Enemy1();
+        Enemy1 enemy3 = new Enemy1();
+
+        int x = Enemy1.enemyCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
